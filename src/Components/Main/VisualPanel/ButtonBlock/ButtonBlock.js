cf753715c1cf183b621ef1ad2f636008d83d2edd
Zaitseva Natalia <zaitsevanatalia7@gmail.com>
2020-01-23 23:49:34 +0200

    from pochaina

--- src/Components/Main/VisualPanel/ButtonBlock/ButtonBlock.js
+++ src/Components/Main/VisualPanel/ButtonBlock/ButtonBlock.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "../../../../../CommonComponens/Button"
+import Button from "../../../../CommonComponens/Button"
 import "./style.scss"
 
 function ButtonBlock() {
